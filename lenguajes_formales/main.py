bfeb3e86813f97525351c6276c6280adb29bb811
Christian_Windows_Desktop <chgeovany.15@gmail.com>
2023-01-22 23:48:50 -0600

    Documentacion

--- lenguajes_formales/main.py
+++ lenguajes_formales/main.py
@@ -1,12 +1,24 @@
 def power(L, n):
+    """
+    Esta función toma como entrada un lenguaje "L" y una potencia "n", 
+    y devuelve el lenguaje elevado a la potencia "n" en forma de un conjunto.
+    """
     if n == 0:
         return set([""])
     elif n == 1:
         return L
     else:
+        """
+        Utiliza una comprensión de lista para generar un nuevo conjunto de palabras combinando
+        cada palabra del lenguaje con cada palabra de la potencia "n-1" del lenguaje.
+        """
         return set([x + y for x in L for y in power(L, n-1)])
 
 def sort(L):
+    """
+    Esta función toma como entrada un lenguaje "L" y devuelve el mismo lenguaje ordenado 
+    alfabéticamente y por longitud de las palabras.
+    """
     return sorted(L, key=lambda x: (len(x), x))
 
 def main():
